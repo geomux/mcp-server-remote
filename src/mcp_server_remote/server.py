f46d9ce2afc5284043d601ba55af863d1328b645
geomux <Henry.LawrenceGEO@gmail.com>
2026-06-21 15:30:48 -0700

    feat: write initial code for server.py, loads config file, initialized MCP server instance, configures server, registers tools, runs server

---
+++ src/mcp_server_remote/server.py
@@ -0,0 +1,40 @@
+# server.py
+# Uses MCP (within Streamable HTTP packets) to coordinate with MCP client
+# Entry point for remote connections, loads config, opens a session, runs tools, returns messages to client.
+### ...lots of notes below because dense...
+
+from fastmcp import FastMCP
+from mcp_server_remote.config_loader import config_load
+from mcp_server_remote import resolve_auth
+from mcp_server_remote import register_tools
+
+### Build the server, set up configuration, register tools, begin listening for client
+def main():
+    """Run MCP Server Instance, open entry point for remote connections, load config, open a session, run tools, return messages to client"""
+    config = config_load() # config file dictionary
+    server = config["server"] # server table (inside config file dict)
+
+    print("_"*50)
+    print(f"Starting {server['name']} on {server['host']}:{server['port']}{server['path']}.")
+    print("_"*50)
+
+    ### ---------------------------
+    ### --- MCP Server Instance ---
+    ### ---------------------------
+    mcp_server = FastMCP(
+        name=server["name"],
+        auth=build_auth(config)
+        )
+
+    register_tools(mcp_server, config) # register available tools
+
+    mcp_server.run(
+        transport="http",
+        host=server['host'],
+        port=server['port'],
+        path=server['path']
+        )
+
+if __name__ == "__main__":
+    main()
+
