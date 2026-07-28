8f504039a33822c9d8e2227a0bca2f68e339301e
geomux <Henry.LawrenceGEO@gmail.com>
2026-07-27 21:40:08 -0700

    fix(src/.../auth.py): added system exit inside if statement for token being less than allowed min length.

--- src/mcp_server_remote/auth.py
+++ src/mcp_server_remote/auth.py
@@ -29,6 +29,7 @@ def resolve_auth(config: dict) -> StaticTokenVerifier:
         print(f"\nToken is {len(token)} characters. Good security needs at least {MIN_TOKEN_LENGTH}.")
         print("\nGenerate an official token: openssl rand -hex 32\n")
         print("_"*50)
+        sys.exit(1)
 
     return StaticTokenVerifier(
         tokens={
